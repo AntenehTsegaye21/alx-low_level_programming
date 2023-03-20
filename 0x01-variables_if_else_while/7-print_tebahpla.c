0c6e2cc563c55c4d1540d8008ea7093ac5639457
AntenehTsegaye <anttseg@gmail.com>
2023-03-20 05:18:27 -0700

    Smile in the mirror. Done!

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,15 @@
-#include <stdio.h>  /**  * main - prints the alphabet in lowercase,  * followed by a new line  * Return: Always 0 (Success)  */ int main(void) {     return (0); }
+#include <stdio.h>
+/**
+ * main - prints the lower case alphabet in reverse,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char st;
+
+	for (st = 'z'; st >= 'a'; st--)
+		putchar(st);
+	putchar('\n');
+	return (0);
+}
