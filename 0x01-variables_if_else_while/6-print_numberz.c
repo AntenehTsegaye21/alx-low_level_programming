d75249ccd72a3c685f5d16c2fe9ec95e27a20e53
AntenehTsegaye <anttseg@gmail.com>
2023-03-20 05:13:04 -0700

    Numberz. Done!

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,15 @@
-#include <stdio.h>  /**  * main - prints the alphabet in lowercase,  * followed by a new line  * Return: Always 0 (Success)  */ int main(void) {     return (0); }
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line, using putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
